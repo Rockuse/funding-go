b9c7232ff388801f62af89e693de762376f4d05b
Rockuse <fahmirockman@gmail.com>
2023-08-16 07:31:40 +0700

    modified:   src/app/user/repository.go

--- src/app/user/repository.go
+++ src/app/user/repository.go
@@ -4,15 +4,9 @@ import "gorm.io/gorm"
 
 type Repository interface {
 	Save(user User) (User, error)
-<<<<<<< HEAD
-	Get(user User) ([]User, error)
-	GetAll(user User) ([]User, error)
-	Delete(user User) (User, error)
-=======
 	// Get(param int) ([]User, error)
 	GetAll(user []User) ([]User, error)
 	// Delete(user User) (User, error)
->>>>>>> 0421883400e7d8f9ec598f63f7e1966b6af40f53
 }
 
 type repository struct {
@@ -33,31 +27,20 @@ func (r *repository) Save(user User) (User, error) {
 
 func (r *repository) Get(param int) ([]User, error) {
 	var users []User
-<<<<<<< HEAD
-	err := r.db.Find(&users, "id = ?", param).Error
-=======
 	err := r.db.Find(&users, "id=?", param).Error
->>>>>>> 0421883400e7d8f9ec598f63f7e1966b6af40f53
 	if err != nil {
 		return users, err
 	}
 	return users, nil
 }
 
-<<<<<<< HEAD
-func (r *repository) GetAll() ([]User, error) {
-	var users []User
-=======
 func (r *repository) GetAll(users []User) ([]User, error) {
->>>>>>> 0421883400e7d8f9ec598f63f7e1966b6af40f53
 	err := r.db.Find(&users).Error
 	if err != nil {
 		return users, err
 	}
 	return users, nil
 }
-<<<<<<< HEAD
-=======
 
 func (r *repository) Delete(users User, param int) (User, error) {
 	err := r.db.Delete(&users, "id=?", param)
@@ -66,4 +49,3 @@ func (r *repository) Delete(users User, param int) (User, error) {
 	}
 	return users, nil
 }
->>>>>>> 0421883400e7d8f9ec598f63f7e1966b6af40f53
