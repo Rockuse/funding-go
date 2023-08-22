8fd4a5f645b3e2bf1c52d775fd9f6c899af277fe
Fahmi Muzakki <fahmirockman@gmail.com>
2023-08-22 19:55:16 +0700

    feat: Add error handling in user

--- src/app/helper/helper.go
+++ src/app/helper/helper.go
@@ -1,5 +1,9 @@
 package helper
 
+import (
+	"github.com/go-playground/validator/v10"
+)
+
 type Response struct {
 	Meta Meta        `json:"meta"`
 	Data interface{} `json:"data"`
@@ -22,4 +26,13 @@ func ResponseHelper(message string, code uint16, status string, data interface{}
 		Data: data,
 	}
 	return ResponseJSON
+}
+
+func FormatValidationError(err error) []string {
+	var errors []string
+	for _, e := range err.(validator.ValidationErrors) {
+		errors = append(errors, e.Error())
+	}
+
+	return errors
 }
