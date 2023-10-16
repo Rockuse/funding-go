c7b97ec7898845c10fe9306b5fd782387d7d3078
Fahmi Muzakki <fahmirockman@gmail.com>
2023-10-16 17:46:00 +0700

    Fix routes

--- src/app/user/routes.go
+++ src/app/user/routes.go
@@ -1,7 +1,6 @@
 package user
 
 import (
-	// "funding/src/middleware"
 	"funding/src/routes"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +10,6 @@ import (
 var UserModule = routes.Module{
 	Name: "User",
 	Routes: func(api *gin.RouterGroup, db *gorm.DB) {
-		// midService := middleware.NewService(db)
 		userRepository := NewRepository(db)
 		userService := NewService(userRepository)
 		userHandler := NewUserHandler(userService)
@@ -21,6 +19,5 @@ var UserModule = routes.Module{
 		userApi.GET("/:id", userHandler.GetUserDataById)
 		userApi.POST("/login", userHandler.Login)
 		userApi.POST("/checkemail", userHandler.CheckEmailAvailibility)
-		// userApi.POST("/avatar", midService.AuthMiddleware(), userHandler.UploadAvatar)
 	},
 }
