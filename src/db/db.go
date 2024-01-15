4123d59b132f8c25605160a350b1d1f3b36e7e40
Fahmi Muzakki <fahmirockman@gmail.com>
2024-01-15 12:52:33 +0700

    update db

--- src/db/db.go
+++ src/db/db.go
@@ -6,8 +6,8 @@ import (
 )
 
 func InitDB() *gorm.DB {
-	dns := "host=satao.db.elephantsql.com user=sxelyaew password=3r9FiVZVfvCtli4eR4ZiL9bb0KYTItW_ dbname=sxelyaew port=5432"
-	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	dsn := "host=satao.db.elephantsql.com user=sxelyaew password=3r9FiVZVfvCtli4eR4ZiL9bb0KYTItW_ dbname=sxelyaew port=5432"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
 	}
